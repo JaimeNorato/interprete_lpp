faca49c4a71b577684062fd2b346216bdb101f45
Jaime Norato <jn901113jn@gmail.com>
2021-05-27 08:16:40 -0500

    test para el llamado de una funcion

--- tests/lexer_test.py
+++ tests/lexer_test.py
@@ -12,11 +12,6 @@ class LexerTest(TestCase):
 
     def test_illegal(self) -> None:
         source: str = '¡¿@'
-        lexer: Lexer = Lexer(source)
-
-        tokens: List[Token] = []
-        for i in range(len(source)):
-            tokens.append(lexer.next_token())
 
         expected_tokens: List[Token] = [
             Token(TokenType.ILLEGAL, '¡'),
@@ -24,45 +19,30 @@ class LexerTest(TestCase):
             Token(TokenType.ILLEGAL, '@')
         ]
 
-        self.assertEquals(tokens, expected_tokens)
+        self.validate_tes(source, expected_tokens)
 
     def test_one_character_operator(self) -> None:
         source: str = '=+'
-        lexer: Lexer = Lexer(source)
-
-        token: List[Token] = []
-        for i in range(len(source)):
-            token.append(lexer.next_token())
 
         expected_tokens: List[Token] = [
             Token(TokenType.ASSIGN, '='),
             Token(TokenType.PLUS, '+'),
         ]
 
-        self.assertEquals(token, expected_tokens)
+        self.validate_tes(source, expected_tokens)
 
     def test_eof(self) -> None:
         source: str = '+'
-        lexer: Lexer = Lexer(source)
-
-        token: List[Token] = []
-        for i in range(len(source) + 1):
-            token.append(lexer.next_token())
 
         expected_tokens: List[Token] = [
             Token(TokenType.PLUS, '+'),
             Token(TokenType.EOF, ''),
         ]
 
-        self.assertEquals(token, expected_tokens)
+        self.validate_tes(source, expected_tokens)
 
     def test_delimiters(self) -> None:
         source = '(){},;'
-        lexer: Lexer = Lexer(source)
-
-        token: List[Token] = []
-        for i in range(len(source)):
-            token.append(lexer.next_token())
 
         expected_tokens: List[Token] = [
             Token(TokenType.RPAREN, '('),
@@ -73,15 +53,10 @@ class LexerTest(TestCase):
             Token(TokenType.SEMICOLON, ';'),
         ]
 
-        self.assertEquals(token, expected_tokens)
+        self.validate_tes(source, expected_tokens)
 
     def test_assignment(self) -> None:
         source = 'variable cinco = 5;'
-        lexer: Lexer = Lexer(source)
-
-        token: List[Token] = []
-        for i in range(5):
-            token.append(lexer.next_token())
 
         expected_tokens: List[Token] = [
             Token(TokenType.LET, 'variable'),
@@ -91,7 +66,7 @@ class LexerTest(TestCase):
             Token(TokenType.SEMICOLON, ';'),
         ]
 
-        self.assertEquals(token, expected_tokens)
+        self.validate_tes(source, expected_tokens)
 
     def test_function_declaration(self) -> None:
         source = '''
@@ -121,10 +96,25 @@ class LexerTest(TestCase):
 
         self.validate_tes(source, expected_tokens)
 
-    # def test_function_call(self) -> None:
-    #     source: str = """
-    #         variable resultado = suma(dos, tres);
-    #     """
+    def test_function_call(self) -> None:
+        source: str = """
+            variable resultado = suma(dos, tres);
+        """
+
+        expected_tokens: List[Token] = [
+            Token(TokenType.LET, 'variable'),
+            Token(TokenType.IDENT, 'resultado'),
+            Token(TokenType.ASSIGN, '='),
+            Token(TokenType.IDENT, 'suma'),
+            Token(TokenType.RPAREN, '('),
+            Token(TokenType.IDENT, 'dos'),
+            Token(TokenType.COMMA, ','),
+            Token(TokenType.IDENT, 'tres'),
+            Token(TokenType.LPAREN, ')'),
+            Token(TokenType.SEMICOLON, ';'),
+        ]
+
+        self.validate_tes(source, expected_tokens)
 
     def validate_tes(self, source: str, expected_tokens: List[Token]) -> None:
         lexer: Lexer = Lexer(source)
